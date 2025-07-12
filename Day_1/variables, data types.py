d355af7cda0716dacfe701cbcaf8dd29de8222f2
Vansh Chhabra <vanshchhabra403@gmail.com>
2025-07-12 16:38:54 +0530

    Create variables, data types.py
    Added Day 1 Python practice

---
+++ Day_1/variables, data types.py
@@ -0,0 +1,37 @@
+#practice questions set
+
+#Write a program to input 2 numbers and print their sum
+
+first= int(input("Enter first:"))
+second= int(input("Enter second:"))
+sum= (first + second)
+
+print("Sum=", sum)
+
+# WAP to input side of a square and print its area
+
+side= int(input("Enter the side of square:"))
+area= side*side
+
+print("Area of the Square=", area)
+
+# #WAP to input 2 floating point numbers and print their avg
+
+num1= float(input("Enter number 1:"))
+num2= float(input("Enter number 2:"))
+
+average= ((num1+num2)/2)
+
+print("The average of the numbers=", average)
+
+#WAP to input 2 int numbers, a and b. Print True if a > or = b. If not print False
+
+a= int(input("Enter number a:"))
+b= int(input("Enter number b:"))
+
+print(a >= b)
+
+
+
+
+
